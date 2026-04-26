0c694aec5814eb1fdfda7330f7282fc1040e9858
Your Name <you@example.com>
2026-04-26 17:35:05 -0400

    feature:footer

--- projects/website/src/app/components/pages/home/home.ts
+++ projects/website/src/app/components/pages/home/home.ts
@@ -5,11 +5,12 @@ import { Hero } from '../../shared/hero/hero';
 import { HpBlocText } from '../../shared/hp-bloc-text/hp-bloc-text';
 import { Hp2Col } from '../../shared/hp-2-col/hp-2-col';
 import { HpWhy } from '../../shared/hp-why/hp-why';
+import { Footer } from '../../../src/app/components/shared/footer/footer';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Button, Loader, Hero, HpBlocText, Hp2Col, HpWhy],
+  imports: [Button, Loader, Hero, HpBlocText, Hp2Col, HpWhy, Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
