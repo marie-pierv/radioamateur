8cb23a31a3c6668f8269c0ac1e925b4bb3bcba6a
Your Name <you@example.com>
2026-05-02 09:43:16 -0400

    Feature: Homepage bloc jaune

--- projects/website/src/app/components/pages/home/home.ts
+++ projects/website/src/app/components/pages/home/home.ts
@@ -4,11 +4,12 @@ import { Loader } from '../../shared/loader/loader';
 import { Hero } from '../../shared/hero/hero';
 import { HpBlocText } from '../../shared/hp-bloc-text/hp-bloc-text';
 import { Hp2Col } from '../../shared/hp-2-col/hp-2-col';
+import { HpWhy } from '../../shared/hp-why/hp-why';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Button, Loader, Hero, HpBlocText, Hp2Col],
+  imports: [Button, Loader, Hero, HpBlocText, Hp2Col, HpWhy],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
---
+++ projects/website/src/app/components/shared/hp-why/hp-why.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Title } from '../title/title';
+
+@Component({
+  selector: 'app-hp-why',
+  imports: [Title],
+  templateUrl: './hp-why.html',
+  styleUrl: './hp-why.scss',
+})
+export class HpWhy {}
