29e4f333d9b51dd41bff971dfea33b068f0ef841
Your Name <you@example.com>
2026-05-02 09:13:01 -0400

    Feature: homepage bloc 2 colonnes

---
+++ projects/website/src/app/components/shared/hp-2-col/hp-2-col.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Title } from '../title/title';
+
+@Component({
+  selector: 'app-hp-2-col',
+  imports: [Title],
+  templateUrl: './hp-2-col.html',
+  styleUrl: './hp-2-col.scss',
+})
+export class Hp2Col {}
