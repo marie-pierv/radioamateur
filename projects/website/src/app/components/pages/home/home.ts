944427f956687ec9bca98ae41a7cf8268cfcb77e
Your Name <you@example.com>
2026-05-04 18:20:20 -0400

    Feature: Création du component animation barres de son

--- projects/website/src/app/components/pages/home/home.ts
+++ projects/website/src/app/components/pages/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Button } from '../../shared/button/button';
 import { Loader } from '../../shared/loader/loader';
 import { Hero } from '../../shared/hero/hero';
+import { AnimationSoundBars } from '../../shared/animation-sound-bars/animation-sound-bars';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Button, Loader, Hero],
+  imports: [Button, Loader, Hero, AnimationSoundBars],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
