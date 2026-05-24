ad1b86995d16663fb90a32f6ad76fb7e46065d5c
Your Name <you@example.com>
2026-05-24 09:42:14 -0400

    Correction de l'action Linting qui n'a pas fonctionnée

--- projects/website/src/app/components/pages/exam-generator/exam-generator.ts
+++ projects/website/src/app/components/pages/exam-generator/exam-generator.ts
@@ -3,13 +3,12 @@ import { Card } from '../../shared/card/card';
 import { ExamDisplay } from '../../shared/exam-display/exam-display';
 import { FormStartExam } from '../../shared/form-start-exam/form-start-exam';
 import { ExamProgress } from '../../shared/exam-progress/exam-progress';
-import { RouterOutlet } from '@angular/router';
 import { PracticeExam } from '../../../services/practice-exam';
 
 @Component({
   selector: 'app-exam-generator',
   standalone: true,
-  imports: [RouterOutlet, FormStartExam, Card, ExamDisplay, ExamProgress],
+  imports: [FormStartExam, Card, ExamDisplay, ExamProgress],
   templateUrl: './exam-generator.html',
   styleUrl: './exam-generator.scss',
 })
